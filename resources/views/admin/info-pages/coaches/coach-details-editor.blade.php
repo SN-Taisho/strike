7a0cca81d4cb25f0cae3cb64124ddf4149c2b9a8
Anzel Sakamoto <anzelkensakamoto@yahoo.com.ph>
2024-01-10 23:43:09 +0800

    Minor adjustments to the programme details editor page

---
+++ resources/views/admin/info-pages/coaches/coach-details-editor.blade.php
@@ -0,0 +1,15 @@
+@include('admin.components.admin-head', ['title' => 'Coeaches Management'])
+
+@include('admin.components.admin-header')
+
+<main>
+    <form action="">
+
+    </form>
+</main>
+
+<script>
+
+</script>
+
+@include('components.footer')
